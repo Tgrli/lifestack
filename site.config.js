1ab56f24c2579427e69c4be4ae3cad5bb7132145
tgr <kl3010@nyu.edu>
2023-09-05 20:46:54 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "The Life Stack",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Founder @Diligen.ai | The Life Stack",
+    bio: "I build things.",
+    email: "Hi@kyan.li",
+    linkedin: "Kai Li",
+    github: "Tgrli",
+    airchat: "Conjecture",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Diligen.ai`,
+      href: "diligen.ai",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "The Life Stack",
+    description: "welcome to the life stack!",
   },
 
   // CONFIG configration (required)
